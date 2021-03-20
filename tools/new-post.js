da786563b4ddb7561d340655d92d665db1c8fa97
phil-willis <phil.willis@gmail.com>
2021-03-20 14:57:00 -0700

    bug fix and added a scipt to build new post

---
+++ tools/new-post.js
@@ -0,0 +1,52 @@
+const fs = require('fs-extra')
+const inquirer = require('inquirer')
+
+const mdTemplate = ({ title, excerpt }) => `---
+title: '${title}'
+excerpt: '${excerpt}'
+coverImage: '/assets/blog/hello-world/cover.jpg'
+ogImage:
+  url: '/assets/blog/hello-world/cover.jpg'
+---
+
+
+# Overview of ${title}
+
+`
+
+const createNewPost = (title, excerpt) => {
+  const titleFormatted = title.replace(/\s+/g, '_')
+  const DIR_PATH_PREFIX = '_posts'
+  const DIR_PATH = `${DIR_PATH_PREFIX}/${titleFormatted}`
+
+  if (!fs.existsSync()) {
+    fs.mkdirSync(DIR_PATH)
+
+    const fileName = `${DIR_PATH}.md`
+    const fileContent = mdTemplate({ title, excerpt })
+    fs.writeFileSync(fileName, fileContent, (err) => {
+      if (err) throw err
+      console.log('The file was succesfully saved!')
+    })
+  }
+}
+
+// Prompt types: [list, rawlist, expand, checkbox, confirm, input, password, editor]
+const promptQuestions = [
+  {
+    type: 'command',
+    name: 'title',
+    message: 'Short title',
+  },
+  {
+    type: 'command',
+    name: 'excerpt',
+    message: 'Give an excerpt',
+  },
+]
+
+inquirer.prompt(promptQuestions).then((answers) => {
+  const { title, excerpt } = answers
+  const shortTitle = title.substring(0, 20)
+  createNewPost(shortTitle, excerpt)
+})
